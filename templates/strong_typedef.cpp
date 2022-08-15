972ee95adb34218a4dfbf6eebbec838b81488f6b
IgorHersht <33009495+IgorHersht@users.noreply.github.com>
2022-08-15 18:36:12 -0400

    Add files via upload

--- templates/strong_typedef.cpp
+++ templates/strong_typedef.cpp
@@ -1,14 +1,31 @@
 #include <map>
 #include <assert.h>
 #include<string>
+#include<string_view>
+#include<array>
+#include<type_traits>
 
-template <typename T, typename U> struct strong_typedef{
-    explicit constexpr strong_typedef( T value ):m_value(value){}
-    auto operator<=> (const strong_typedef& ) const = default;// impicitly constexpr
-    constexpr T value() const {
+template<typename T> concept ShouldBeCopied = std::is_trivially_copy_assignable_v<T>;
+template<typename T> concept ShouldBeMoved = !ShouldBeCopied<T>;
+
+template < typename T, typename U>
+struct strong_typedef{
+
+    explicit constexpr strong_typedef( T value ) requires (ShouldBeCopied<T>)
+            :m_value(value){
+        int i =1;
+    }
+    explicit constexpr strong_typedef( T&& value ) requires (ShouldBeMoved<T>)
+            :m_value(std::move(value)){
+        int i =1;
+
+    }
+
+    auto operator<=> (const strong_typedef& ) const = default;
+    constexpr T value() const requires (ShouldBeCopied<T>) {
         return m_value;
     }
-    constexpr const T& const_ref() const {
+    constexpr const T& const_ref() const requires (ShouldBeMoved<T>) {
         return m_value;
     }
 private:
@@ -16,36 +33,47 @@ private:
 };
 
 
+
+std::array ar1{1,2};
+std::array ar2{1,3};
+using ArOne = strong_typedef<decltype(ar1), struct One>;
+using ArTwo = strong_typedef<decltype(ar2), struct Two>;
+
 using IntOne = strong_typedef<int, struct One>;
 using IntTwo = strong_typedef<int, struct Two>;
 using StringThree = strong_typedef<std::string, struct Three>;
 using StringFour = strong_typedef<std::string, struct Four>;
 
-void foo(IntOne intOne, IntTwo intTwo, StringThree stringThree, StringFour stringFour ){
+void foo(IntOne intOne, IntTwo intTwo, StringThree stringThree, StringFour stringFour, ArOne arOne, ArTwo arTwo  ){
     //intOne < intTwo ; // error
     //(intOne != intTwo ); // error
-   // IntOne i1 = intTwo; // error
+    // IntOne i1 = intTwo; // error
     //intOne = intTwo; // error
     //IntOne i1 = 5; // error
     //intOne =5; // error
-   //stringThree != stringFour ;// error
+    //stringThree != stringFour ;// error
     //intOne != intTwo ;
-    std::string s3 = stringThree.value();
+    std::string s3 = stringThree.const_ref();
     const std::string& src =stringFour.const_ref();
     int i =1;
 }
 
 
 
+
 int main(){
     IntOne i1(1);
     std::string s3("3");
-    foo(i1, IntTwo(2), StringThree(std::move(s3)), StringFour("4"));
- //   foo(IntTwo(2), i1,  StringThree(std::move(s3)), StringFour("4")); // error
+    ArOne arOne(ar1);
+    foo(i1, IntTwo(2), StringThree(std::move(s3)), StringFour("4"), arOne, ArTwo(ar2) );
+    //   foo(IntTwo(2), i1,  StringThree(std::move(s3)), StringFour("4"), arOne, ArTwo(ar2)); // error
+    //foo(i1, IntTwo(2), StringThree(std::move(s3)), StringFour("4"), ArTwo(ar2), arOne  );
 
     std::map<StringThree, int> m1;
     m1.try_emplace(StringThree("5"), 25);
     constexpr auto r = IntOne(5) <=> IntOne(6);
+
+
     int i =1;
 
 }
