c5c8c4ed57328c1115c8f3aaac9ef17d90250a25
IgorHersht <33009495+IgorHersht@users.noreply.github.com>
2024-08-02 14:36:45 -0400

    Add files via upload

---
+++ templates/tuple/for_each_tuple2.cpp
@@ -0,0 +1,84 @@
+
+#include <tuple>
+#include <concepts>
+
+template <typename T>
+struct IsTupleImpl : std::false_type {};
+
+template <typename... U>
+struct IsTupleImpl<std::tuple <U...>> : std::true_type {};
+
+template <typename T> constexpr static bool is_tuple_v = IsTupleImpl<std::decay_t<T>>::value;
+
+template <typename TupleT,  typename Fn>
+void for_each(TupleT&& tp, Fn&& fn) requires is_tuple_v<TupleT>  {
+    std::apply (
+        [&fn]<typename ...T>(T&& ...args) requires (... && std::invocable<Fn, T>) {
+            (std::forward<Fn>(fn)(std::forward<T>(args)), ...);
+        }, std::forward<TupleT>(tp)
+    );
+}
+
+/// test
+#// https://www.cppstories.com/2022/tuple-iteration-apply/
+#include <tuple>
+#include <concepts>
+
+template <typename T>
+struct IsTupleImpl : std::false_type {};
+
+template <typename... U>
+struct IsTupleImpl<std::tuple <U...>> : std::true_type {};
+
+template <typename T> constexpr static bool is_tuple_v = IsTupleImpl<std::decay_t<T>>::value;
+
+template <typename TupleT,  typename Fn>
+void for_each(TupleT&& tp, Fn&& fn) requires is_tuple_v<TupleT>  {
+    std::apply (
+        [&fn]<typename ...T>(T&& ...args) requires (... && std::invocable<Fn, T>) {
+            (std::forward<Fn>(fn)(std::forward<T>(args)), ...);
+        }, std::forward<TupleT>(tp)
+    );
+}
+
+/// test
+#include <string>
+#include <iostream>
+#include <assert.h>
+
+auto increment1 = []( auto& t) { ++t ; };
+
+struct increment2 {
+    void operator()(auto& t) { ++t;}
+};
+
+
+int main() {
+    std::tuple<int, char> t1{ 10, 'a'};
+
+    for_each(t1, increment1);
+    assert(std::get<0>(t1) == 11);
+    assert(std::get<1>(t1) == 'b');
+    for_each(t1, increment2());
+    assert(std::get<0>(t1) == 12);
+    assert(std::get<1>(t1) == 'c');
+
+    const int increase = 2; bool status = true;
+
+    auto addWithTest =  [increase, &status] ( auto& t) {
+        t += increase;
+        if(int(t) > int('e')) {
+            status = false;
+        }
+    };
+
+    for_each(t1, addWithTest);
+    assert(std::get<0>(t1) == 14);
+    assert(status);
+    for_each(t1, addWithTest);
+    assert(std::get<0>(t1) == 16);
+    assert(!status);
+
+}
+
+
---
+++ templates/tuple/tuple_lauout.cpp
@@ -0,0 +1,46 @@
+
+#include <tuple>
+#include <array>
+#include <cstring>
+#include <string>
+#include <memory>
+#include <assert.h>
+
+template <typename... TS>  struct is_tuple :public std::false_type {};
+template <typename... TS> struct is_tuple<std::tuple<TS...>> :std::true_type {};// specilization on tuple
+template <typename... TS> constexpr static bool is_tuple_v = is_tuple<TS...>::value;
+template <typename... TS> concept same_as_tuple = is_tuple_v<TS...>;
+
+template<size_t Idx,same_as_tuple T>
+ size_t tuple_element_offset() {
+    return static_cast<size_t>(
+                reinterpret_cast<char*>(&std::get<Idx>(*reinterpret_cast<T*>(0))) - reinterpret_cast<char*>(0));
+}
+
+template<typename ET,same_as_tuple T>
+ size_t tuple_element_offset() {
+    return static_cast<size_t>(
+                reinterpret_cast<char*>(&std::get<ET>(*reinterpret_cast<T*>(0))) - reinterpret_cast<char*>(0));
+}
+//// test
+struct C {
+    char ch = 'x';
+    std::array<int,5> ar{3, 7, 25, 4, 88 };
+    double d{0.45};
+};
+// reverse order!!!
+using MyTuple = std::tuple< double, std::array<int,5>, char >;
+
+int main() {
+    static_assert(sizeof(MyTuple)== sizeof(C));
+    MyTuple tuple;
+    C c1;
+    memcpy(&tuple,  &c1, sizeof(C));
+
+    assert(offsetof(C,d) == (tuple_element_offset<0, MyTuple>()) );
+    assert(offsetof(C,ar) == (tuple_element_offset<1, MyTuple>()) );
+    assert(offsetof(C,ch) == (tuple_element_offset<2, MyTuple>()) );
+
+}
+
+
